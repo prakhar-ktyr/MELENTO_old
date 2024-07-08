6079f093e94d4ab1c68658b6da8154abd3c832c7
prakhar-ktyr <katiyar.4@iitj.ac.in>
2024-07-08 09:39:01 +0530

    chore: Update server.js to use bodyParser for parsing JSON and urlencoded data

--- MELENTO_Server/server.js
+++ MELENTO_Server/server.js
@@ -1,21 +1,21 @@
-const assessments_controller = require('./controller/assessments_controller');
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const assessments_controller = require('./controller/assessments_controller');
 
 const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = 3000;
-const cors = require('cors');
-app.use(cors());
-app.use(express.json());
-const server = app.listen(port, () => 
-    console.log(`Server running on port ${port}`)
-);
-
-app.get('/assessments', (req, res) => {
-    assessments_controller.getAssessments(req, res);
+const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
 
-app.post('/addAssessment', (req, res) => {
-    assessments_controller.addAssessment(req, res);
-});
+app.get('/assessments', assessments_controller.getAssessments);
+app.get('/assessments/:id', assessments_controller.getAssessmentById);
+app.post('/addAssessment', assessments_controller.addAssessment);
+app.put('/assessments/:id', assessments_controller.updateAssessment);
+app.delete('/assessments/:id', assessments_controller.deleteAssessment);
