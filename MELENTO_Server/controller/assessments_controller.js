7f892e1463fad5a4e0c1ff0f82cfa4ab943b92c3
prakhar-ktyr <katiyar.4@iitj.ac.in>
2024-07-08 09:36:42 +0530

    chore: Refactor assessments controller to use async/await and improve error handling

--- MELENTO_Server/controller/assessments_controller.js
+++ MELENTO_Server/controller/assessments_controller.js
@@ -1,28 +1,57 @@
+const assessments_service = require('../services/assessment_service');
 const util = require('../util/util');
-const assessments_service = require('../services/asessment_service');
+
+module.exports = {
+    getAssessments,
+    getAssessmentById,
+    addAssessment,
+    updateAssessment,
+    deleteAssessment
+};
 
 function getAssessments(req, res) {
     assessments_service.findAssessmentsAll().then(
         (items) => {
-            console.log("Promise fulfilled")
+            console.log("Promise fulfilled");
             console.log(items);
             const objArr = items;
             objArr.forEach((obj) => {
                 util.renamekey(obj, "_id", "id");
             });
-            const updateItems = JSON.stringify(objArr);
-            console.log(updateItems);
+            const updatedItems = JSON.stringify(objArr);
+            console.log(updatedItems);
             res.setHeader('Content-Type', 'application/json');
-            res.send(updateItems);
-
+            res.send(updatedItems);
         },
         (err) => {
-            console.error("Promise ocurred");
-
+            console.error("Promise failed");
+            res.status(500).json({ message: err.message });
         }
     ).catch((error) => {
-        console.error("Could not get products: ", error);
-    });;  //.catch not required, if error comes both will be shown
+        console.error("Could not get assessments: ", error);
+        res.status(500).json({ message: error.message });
+    });
+}
+
+function getAssessmentById(req, res) {
+    assessments_service.findAssessmentById(req.params.id).then(
+        (item) => {
+            if (item) {
+                util.renamekey(item, "_id", "id");
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(item));
+            } else {
+                res.status(404).json({ message: 'Assessment not found' });
+            }
+        },
+        (err) => {
+            console.error("Promise failed");
+            res.status(500).json({ message: err.message });
+        }
+    ).catch((error) => {
+        console.error("Could not get assessment: ", error);
+        res.status(500).json({ message: error.message });
+    });
 }
 
 function addAssessment(req, res) {
@@ -44,14 +73,56 @@ function addAssessment(req, res) {
             res.send(result);
         },
         (err) => {
-            console.error("Promise ocurred");
+            console.error("Promise failed");
+            res.status(500).json({ message: err.message });
         }
     ).catch((error) => {
-        console.error("Could not add product: ", error);
-    });;
+        console.error("Could not add assessment: ", error);
+        res.status(500).json({ message: error.message });
+    });
 }
 
-module.exports = {
-    getAssessments,
-    addAssessment
-};
+function updateAssessment(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log("method called");
+    console.log(req.body);
+    assessments_service.updateAssessment(req.params.id, req.body).then(
+        (result) => {
+            if (result) {
+                console.log("Promise fulfilled");
+                res.send(result);
+            } else {
+                res.status(404).json({ message: 'Assessment not found' });
+            }
+        },
+        (err) => {
+            console.error("Promise failed");
+            res.status(500).json({ message: err.message });
+        }
+    ).catch((error) => {
+        console.error("Could not update assessment: ", error);
+        res.status(500).json({ message: error.message });
+    });
+}
+
+function deleteAssessment(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log("method called");
+    assessments_service.deleteAssessment(req.params.id).then(
+        (result) => {
+            if (result) {
+                console.log("Promise fulfilled");
+                res.json({ message: 'Assessment deleted' });
+            } else {
+                res.status(404).json({ message: 'Assessment not found' });
+            }
+        },
+        (err) => {
+            console.error("Promise failed");
+            res.status(500).json({ message: err.message });
+        }
+    ).catch((error) => {
+        console.error("Could not delete assessment: ", error);
+        res.status(500).json({ message: error.message });
+    });
+}
