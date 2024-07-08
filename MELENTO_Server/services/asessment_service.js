cadf1078f506e757ebd6dd8aacf6e8a53d464efc
prakhar-ktyr <katiyar.4@iitj.ac.in>
2024-07-08 09:32:37 +0530

    chore: Refactor assessment service to use async/await and improve error handling

--- MELENTO_Server/services/asessment_service.js
+++ MELENTO_Server/services/asessment_service.js
@@ -1,29 +1,58 @@
 var util = require('../util/util');
 
+// Helper function to add an object to the collection
 function addObject(collection, object) {
-    collection.insertOne(object, function (err, result) {
-        if (!err) {
-            console.log("Object added");
-            console.log(result);
-        }
+    return new Promise((resolve, reject) => {
+        collection.insertOne(object, function (err, result) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Object added");
+                console.log(result);
+                resolve(result);
+            }
+        });
     });
 }
 
+// Find all assessments
 var findAssessmentsAll = function () {
     console.log("findAssessmentsAll");
     return new Promise(async (resolve, reject) => {
-        var coll = util.connect("assessments");
-        var items = coll.find().toArray();
-        console.log(items);
-        resolve(items);
+        var coll = await util.connect("assessments");
+        coll.find().toArray(function (err, items) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(items);
+                resolve(items);
+            }
+        });
     });
 }
 
+// Find assessment by ID
+var findAssessmentById = function (id) {
+    console.log("findAssessmentById");
+    return new Promise(async (resolve, reject) => {
+        var coll = await util.connect("assessments");
+        coll.findOne({ _id: id }, function (err, item) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(item);
+                resolve(item);
+            }
+        });
+    });
+}
+
+// Add a new assessment
 function addAssessment(id, assessmentName, assessmentDescription, assessmentImage, questions, price, facultyId, time, isActive) {
-    return new Promise((resolve, reject) => {
-        var coll = util.connect("assessments");
+    return new Promise(async (resolve, reject) => {
+        var coll = await util.connect("assessments");
         resolve(addObject(coll, {
-            id: id,
+            _id: id,
             assessmentName: assessmentName,
             assessmentDescription: assessmentDescription,
             assessmentImage: assessmentImage,
@@ -32,11 +61,46 @@ function addAssessment(id, assessmentName, assessmentDescription, assessmentImag
             facultyId: facultyId,
             time: time,
             isActive: isActive
-        }))
+        }));
+    });
+}
+
+// Update an assessment
+function updateAssessment(id, updatedAssessment) {
+    return new Promise(async (resolve, reject) => {
+        var coll = await util.connect("assessments");
+        coll.updateOne({ _id: id }, { $set: updatedAssessment }, function (err, result) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Assessment updated");
+                console.log(result);
+                resolve(result);
+            }
+        });
+    });
+}
+
+// Delete an assessment
+function deleteAssessment(id) {
+    return new Promise(async (resolve, reject) => {
+        var coll = await util.connect("assessments");
+        coll.deleteOne({ _id: id }, function (err, result) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Assessment deleted");
+                console.log(result);
+                resolve(result);
+            }
+        });
     });
 }
 
 module.exports = {
     findAssessmentsAll,
-    addAssessment
+    findAssessmentById,
+    addAssessment,
+    updateAssessment,
+    deleteAssessment
 };
